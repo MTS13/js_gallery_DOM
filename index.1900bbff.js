26eb618be8493559ba829540355895e12307bd00
MTS13 <mtstaras@gmail.com>
2024-07-06 15:08:28 +0300

    make build

---
+++ index.1900bbff.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");e.preventDefault(),t.src=r.href});
+//# sourceMappingURL=index.1900bbff.js.map
